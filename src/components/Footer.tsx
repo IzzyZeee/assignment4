27b183f6a66f33ebd44f66605b08e01e3ac04079
unknown <342896818@TDSB.ON.CA>
2026-04-22 10:53:57 -0400

    Made footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,20 @@
 import { Link } from '@/components';
+import { FaGithub } from "react-icons/fa";
+import { GITHUB_LINK } from '@/core/constants';
 
 export const Footer = () => {
   return (
-    <header>
-        <nav className="flex gap-4 p-4 bg-zinc-800">
-        <h1 className="text-2xl font-bold text-white-900">TMDB Explorer</h1>
+    <div>
+      <nav className="flex gap-4 p-4 bg-zinc-800">
+        <div className="text-xl text-white-900 flex justify-center items-center">
+          <div className="mt-1 mr-3">
+            <FaGithub />
+          </div>
+          <div>
+            <a href={GITHUB_LINK} target="_blank" rel="noopener noreferrer">GitHub</a>
+          </div>
+        </div>
       </nav>
-    </header>
+    </div>
   );
 };
