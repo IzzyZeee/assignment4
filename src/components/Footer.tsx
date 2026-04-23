b4c6c3c0a21785cb102c1be89a57e8ad2ec5de88
Zhang <342896818@tdsb.ca>
2026-04-23 09:19:26 -0400

    Footer saved and fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ import { GITHUB_LINK } from '@/core/constants';
 export const Footer = () => {
   return (
     <div>
-      <nav className="flex gap-4 p-4 bg-zinc-800">
-        <div className="text-xl text-white-900 flex justify-center items-center">
-          <div className="mt-1 mr-3">
+      <nav className="gap-4 p-4 bg-zinc-800">
+        <div className="text-10 text-white-900 flex justify-center items-center">
+          <div className="mr-3">
             <FaGithub />
           </div>
           <div>
--- src/core/constants.ts
+++ src/core/constants.ts
@@ -7,3 +7,4 @@ export const MOVIE_ENDPOINT = 'https://api.themoviedb.org/3/movie';
 export const NOW_PLAYING_ENDPOINT = 'https://api.themoviedb.org/3/movie/now_playing';
 export const TRENDING_ENDPOINT = 'https://api.themoviedb.org/3/trending/movie';
 export const SEARCH_ENDPOINT = 'https://api.themoviedb.org/3/search/person';
+export const GITHUB_LINK = 'https://github.com/IzzyZeee/assignment4.git';
